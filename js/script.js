6d2c6b64165ec2288aae286ef3879b5afc61a402
stappost <emanuele.scarpantoni@gmail.com>
2023-11-20 15:36:06 +0100

    add var price and prompt in js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// PROMPT KM E ETA 
+let km = prompt("inserire kilometri che si desidera percorrere");
+let eta = prompt("inserire la propria eta'")
+// VARIABILI PREZZO VIAGGI E TOTALE 
+let prezzo_viaggio = 0.21
+let prezzo_totale = prezzo_viaggio * km
